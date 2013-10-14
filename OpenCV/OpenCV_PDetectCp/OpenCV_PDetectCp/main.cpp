68f52fcb7b00d5fe41314330371c60f940c24409
Kousuke Tada <pekaso34@gmail.com>
2013-10-14 19:13:21 +0300

    add some comments(Template matching don't work)

--- OpenCV/OpenCV_PDetectCp/OpenCV_PDetectCp/main.cpp
+++ OpenCV/OpenCV_PDetectCp/OpenCV_PDetectCp/main.cpp
@@ -14,8 +14,19 @@ int main(int argc, const char * argv[])
     std::vector<std::vector<cv::Point>> tmp_str;
     std::vector<std::vector<cv::Point>>::iterator itc;
     
+    cv::Mat tmp_chr[35];
+    char tmpname[100];
+    
+    //Load Template image
+    for (int i = 1; i < 35; i++) {
+        std::sprintf(tmpname, "/Users/Kousuke/LPR-Project/OpenCV/OpenCV_PDetectCp/OpenCV_PDetectCp/Template/%d.bmp",i);
+        tmp_chr[i] = cv::imread(tmpname, CV_LOAD_IMAGE_GRAYSCALE);
+        //cv::Canny(tmp_chr[i], tmp_chr[i], 0, 255);
+        //cv::adaptiveThreshold(tmp_chr[i], tmp_chr[i], 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 5, 5);
+    }
+    
     //Load image
-    src_img = cv::imread("/Users/Kousuke/Documents/OpenCV/OpenCV_PDetectCp/OpenCV_PDetectCp/DetectSample/detectsample4.bmp");
+    src_img = cv::imread("/Users/Kousuke/LPR-Project/OpenCV/OpenCV_PDetectCp/OpenCV_PDetectCp/DetectSample/detectsample4.bmp");
     if(src_img.empty()){
         return -1; //If image can't loaded
     }
@@ -87,27 +98,35 @@ int main(int argc, const char * argv[])
     cv::destroyWindow("dst_img1");
     cv::destroyWindow("dst_img3");
     
+    //Find the contours in license plate image.
     cv::findContours(dst_img4, strage, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
     
     int cnt = 0;
+    int offs2 = 2;
     char name[10];
     
     itc = strage.begin();
+    
+    //Compare with aspect rate in all contours
     while (itc!= strage.end()) {
-        
+                //Extract only current contour.
                 std::vector<cv::Point> pointr = *itc;
                 cv::Mat rectMat = cv::Mat(pointr);
-                
+        
+                //Found the rectange surrounding the contour.
                 cv::Rect rect = cv::boundingRect(rectMat);
+        
+                //Calculate the aspect rate. Character aspect rate < 1 and Character size > 30
                 prate = (double)rect.width/rect.height;
                 if (prate >0 && prate <1 && itc -> size() > 30){
-                    //cv::rectangle(dst_img3, cv::Point(rect.x,rect.y), cvPoint (rect.x + rect.width, rect.y + rect.height), CV_RGB (0, 255, 0), 1);
                     
+                    //Crop and resize the character to match the template.
                     cnt++;
-                    chr_tmp = dst_img3(rect);
+                    chr_tmp = dst_img3(cv::Rect(rect.x-offs2,rect.y-offs2,rect.width+2*offs2,rect.height+2*offs2));
                     chr_img[cnt] = cv::Mat::zeros(100, 60, CV_8U);
                     cv::resize(chr_tmp, chr_img[cnt], chr_img[cnt].size());
                     
+                    //Show the characters.
                     std::sprintf(name, "chr_img%d",cnt);
                     std::string wname(name);
                     cv::namedWindow(wname, CV_WINDOW_AUTOSIZE);
@@ -121,30 +140,34 @@ int main(int argc, const char * argv[])
     cv::waitKey();
     
     /*
-    cv::Mat tmp_chr[34];
-    char tmpname[100];
-    
-    for (int i = 1; i < 35; i++) {
-        std::sprintf(tmpname, "/Users/Kousuke/Documents/OpenCV/OpenCV_PDetectCp/OpenCV_PDetectCp/Template/%d.bmp",i);
-        tmp_chr[i] = cv::imread(tmpname, CV_LOAD_IMAGE_GRAYSCALE);
-        //cv::Canny(tmp_chr[i], tmp_chr[i], 0, 255);
-        cv::adaptiveThreshold(tmp_chr[i], tmp_chr[i], 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY_INV, 5, 5);
-    }
-    
     double result[cnt][34];
-    char text[16];
+    //char text[16];
     
     for (int j = 1; j < cnt+1; j++) {
-        cv::findContours(chr_img[j], chr_str, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+        cv::findContours(chr_img[j], chr_str, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
         for (int i = 1; i < 35; i++) {
-            cv::findContours(tmp_chr[i], tmp_str, CV_RETR_TREE, CV_CHAIN_APPROX_NONE);
-            result[j][i] = cv::matchShapes(chr_str[0], chr_str[0], CV_CONTOURS_MATCH_I1, 0);
+            cv::findContours(tmp_chr[i], tmp_str, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+            result[j][i] = cv::matchShapes(tmp_str, chr_str, CV_CONTOURS_MATCH_I3, 0);
             //std::sprintf(text,"%.5f", result[j][i]);
             std::cout << j << "," << i << "," << result[j][i] << std::endl;
         }
         cv::waitKey();
     }
-    
+    */
+    /*
+    cv::Scalar tmpscalar;
+    double result[cnt][34];
+    //char text[16];
+    cv::Mat tmp;
+    for (int j = 1; j < cnt+1; j++) {
+        for (int i = 1; i < 35; i++) {
+            cv::subtract(chr_img[j], tmp_chr[i], tmp);
+            cv::pow(tmp, 2, tmp);
+            tmpscalar = cv::sum(tmp);
+            result[j][i] = tmpscalar.val[0];
+            std::cout << j << "," << i << "," << result[j][i] << std::endl;
+        }
+    }
     cv::waitKey();
     */
 }
